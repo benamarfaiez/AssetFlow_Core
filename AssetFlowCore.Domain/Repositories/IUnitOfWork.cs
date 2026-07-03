994d08076c85a1450b3f2a816280258bec8820cd
faiez_benamar <38057044+benamarfaiez@users.noreply.github.com>
2026-07-03 13:51:24 +0200

    Suppression de la gestion explicite des transactions
    Les méthodes CommitTransactionAsync et RollbackTransactionAsync ont été retirées de l’interface IUnitOfWork et de son implémentation UnitOfWork. L’unité de travail ne gère plus directement les transactions via ces méthodes.

--- AssetFlowCore.Domain/Repositories/IUnitOfWork.cs
+++ AssetFlowCore.Domain/Repositories/IUnitOfWork.cs
@@ -7,6 +7,4 @@ public interface IUnitOfWork
     ITeamRepository Team { get; }
     IMaintenanceTicketRepository MaintenanceTicket { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
-    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
 }
